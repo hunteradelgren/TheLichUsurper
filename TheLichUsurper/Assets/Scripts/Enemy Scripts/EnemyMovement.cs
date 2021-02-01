b54cb02b40541d03d84e2644c58fd89d163577b7
HunterAdelgren <hunteradelgren@gmail.com>
2021-02-01 01:48:50 -0600

    BasicEnemyMovement
    The Enemy has a target object and will follow that object. You can set the distance to follow from and the movement speed for now.

---
+++ TheLichUsurper/Assets/Scripts/Enemy Scripts/EnemyMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField]
+    Transform target; //the target object's Transform
+
+    [SerializeField]
+    float targetDist; //how close to the target will the enemy try to get
+
+    [SerializeField]
+    float movSpeed; //movement speed
+
+    private float distance; //will hold the distance between the target and the enemy
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checks for whether there is a target
+        if (target == null)
+            return;
+
+        distance = Vector3.Distance(target.position, transform.position); //finds the distance from the target location
+        //checks to see if the enemy is within stopping range and returns if so
+        if (distance <= targetDist)
+            return;
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, movSpeed * Time.deltaTime); //moves towards the target
+    }
+}
