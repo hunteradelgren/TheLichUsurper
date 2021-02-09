80267ed85cc294d0ecf1b3251c30340a8d1cf21b
HunterAdelgren <hunteradelgren@gmail.com>
2021-02-08 21:29:49 -0600

    Basic Player movement. Added a script for rotating a head sprite to look at the mouse position.
    Basic Player movement. Added a script for rotating a head sprite to look at the mouse position. Also, fixed some issues with rotations on enemy projecitles.

--- TheLichUsurper/Assets/Scripts/Enemy Scripts/RangedAttack.cs
+++ TheLichUsurper/Assets/Scripts/Enemy Scripts/RangedAttack.cs
@@ -108,7 +108,7 @@ public class RangedAttack : MonoBehaviour
         //moves the projectile on top of the enemy
         projectile.transform.position = transform.position + transform.forward;
         //rotates projectile to where the enemy is facing
-        projectile.transform.rotation = transform.rotation;
+        projectile.transform.rotation = Quaternion.Euler(90, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
     }
 }
 
