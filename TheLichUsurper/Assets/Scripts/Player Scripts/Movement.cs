3eea90e972bf320f0592503bdf48f039a6bc476e
Revan-Saber <cmcadow@my.lonestar.edu>
2021-02-14 12:11:03 -0600

    initial push
    Got character to follow mouse and move indipentantly, head lights up when firing

---
+++ TheLichUsurper/Assets/Scripts/Player Scripts/Movement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed; //player's movement speed
+
+    //stores key inputs
+    private float horizontalAxis; 
+    private float verticalAxis;
+    int floor;
+    Rigidbody prb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Awake()
+    {
+        floor = LayerMask.GetMask("Quad");
+        prb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal"); //gets the horizontal input
+        verticalAxis = Input.GetAxis("Vertical"); //gets the vertical input
+        transform.Translate(new Vector3(horizontalAxis, 0, verticalAxis) * moveSpeed * Time.deltaTime); //moves in direction of the input
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        if(Physics.Raycast(camRay, out floorHit, 100f, floor))
+        {
+            Vector3 mousePoint = floorHit.point - transform.position;
+            mousePoint.y = 0f;
+            Quaternion newRot = Quaternion.LookRotation(mousePoint);
+            prb.MoveRotation(newRot);
+        }
+    }
+}
