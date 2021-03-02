4af6ffe3cba20e59515e0a2f733119757221808e
SirKornholio <54858728+SirKornholio@users.noreply.github.com>
2021-03-02 17:20:28 -0600

    Tweaked more dungeon and movement code

--- TheLichUsurper/Assets/Scripts/Player Scripts/Movement.cs
+++ TheLichUsurper/Assets/Scripts/Player Scripts/Movement.cs
@@ -13,7 +13,7 @@ public class Movement : MonoBehaviour
     int floor;
     Rigidbody prb;
 
-    
+    public float loadingTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -28,17 +28,23 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalAxis = Input.GetAxis("Horizontal"); //gets the horizontal input
-        verticalAxis = Input.GetAxis("Vertical"); //gets the vertical input
-        transform.Translate(new Vector2(horizontalAxis, verticalAxis) * moveSpeed * Time.deltaTime); //moves in direction of the input
-        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit floorHit;
-        if(Physics.Raycast(camRay, out floorHit, 100f, floor))
+        if (loadingTimer <= 0)
         {
-            Vector3 mousePoint = floorHit.point - transform.position;
-            mousePoint.y = 0f;
-            Quaternion newRot = Quaternion.LookRotation(mousePoint);
-            prb.MoveRotation(newRot);
+            horizontalAxis = Input.GetAxis("Horizontal"); //gets the horizontal input
+            verticalAxis = Input.GetAxis("Vertical"); //gets the vertical input
+            transform.Translate(new Vector2(horizontalAxis, verticalAxis) * moveSpeed * Time.deltaTime); //moves in direction of the input
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit floorHit;
+            if (Physics.Raycast(camRay, out floorHit, 100f, floor))
+            {
+                Vector3 mousePoint = floorHit.point - transform.position;
+                mousePoint.y = 0f;
+                Quaternion newRot = Quaternion.LookRotation(mousePoint);
+                prb.MoveRotation(newRot);
+            }
         }
+        else
+            loadingTimer -= Time.deltaTime;
     }
 }
+
