a09b345a369dd98a3cf00446a7e60dbfc3faa3f6
Revan-Saber <cwmcadow@gmail.com>
2021-03-07 13:17:28 -0600

    3-7 UI improvements
    Pause menu works, and a loading canvas had been added.

--- TheLichUsurper/Assets/Scripts/Player Scripts/Movement.cs
+++ TheLichUsurper/Assets/Scripts/Player Scripts/Movement.cs
@@ -12,7 +12,7 @@ public class Movement : MonoBehaviour
     private float verticalAxis;
     int floor;
     private Rigidbody prb;
-    
+    public bool status;
     public Animator animator;
     
     public float loadingTimer;
@@ -36,50 +36,69 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = 0;
-
-        Vector3 objectPos = cam.WorldToScreenPoint(transform.position);
-        mousePos.x = mousePos.x - objectPos.x;
-        mousePos.y = mousePos.y - objectPos.y;
-
-        angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-
-        
-
-
-        HitPoint.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle+90));
-        if (angle < 0)
-            animator.SetFloat("PlayerRot", angle + 359);
-        else
-            animator.SetFloat("PlayerRot", angle);
-
-        if (loadingTimer <= 0)
+        if (Time.timeScale != 0)
         {
-            horizontalAxis = Input.GetAxis("Horizontal"); //gets the horizontal input
-            verticalAxis = Input.GetAxis("Vertical"); //gets the vertical input
-            transform.Translate(new Vector2(horizontalAxis, verticalAxis) * moveSpeed * Time.deltaTime,Space.World); //moves in direction of the input
-            /*Ray camRay = cam.ScreenPointToRay(Input.mousePosition);
-            //RaycastHit floorHit;
-            Plane groundPlane = new Plane(Vector3.right, Vector3.zero);
-            float raylength;
-            if (groundPlane.Raycast(camRay, out raylength))
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 0;
+
+            Vector3 objectPos = cam.WorldToScreenPoint(transform.position);
+            mousePos.x = mousePos.x - objectPos.x;
+            mousePos.y = mousePos.y - objectPos.y;
+
+            angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+
+
+
+            HitPoint.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 90));
+            if (angle < 0)
+                animator.SetFloat("PlayerRot", angle + 359);
+            else
+                animator.SetFloat("PlayerRot", angle);
+
+            if (loadingTimer <= 0)
             {
-                Vector3 pointToLook = camRay.GetPoint(raylength);
-                Debug.DrawLine(camRay.origin, pointToLook, Color.blue);
-                transform.LookAt(pointToLook);
+                horizontalAxis = Input.GetAxis("Horizontal"); //gets the horizontal input
+                verticalAxis = Input.GetAxis("Vertical"); //gets the vertical input
+                transform.Translate(new Vector2(horizontalAxis, verticalAxis) * moveSpeed * Time.deltaTime, Space.World); //moves in direction of the input
+                /*Ray camRay = cam.ScreenPointToRay(Input.mousePosition);
+                //RaycastHit floorHit;
+                Plane groundPlane = new Plane(Vector3.right, Vector3.zero);
+                float raylength;
+                if (groundPlane.Raycast(camRay, out raylength))
+                {
+                    Vector3 pointToLook = camRay.GetPoint(raylength);
+                    Debug.DrawLine(camRay.origin, pointToLook, Color.blue);
+                    transform.LookAt(pointToLook);
 
 
 
 
-                /*Vector3 mousePoint = floorHit.point - transform.position;
-                mousePoint.y = 0f;
-                Quaternion newRot = Quaternion.LookRotation(mousePoint);
-                prb.MoveRotation(newRot);
-            }*/
+                    /*Vector3 mousePoint = floorHit.point - transform.position;
+                    mousePoint.y = 0f;
+                    Quaternion newRot = Quaternion.LookRotation(mousePoint);
+                    prb.MoveRotation(newRot);
+                }*/
+            }
+            else
+                loadingTimer -= Time.deltaTime;
+        }
+    }
+    public void upgrade()
+    {
+        status = !status;
+
+        if (status)
+        {
+            moveSpeed += 5;
+            print("speed upped");
         }
         else
-            loadingTimer -= Time.deltaTime;
+        {
+            moveSpeed -= 5;
+            print("speed downed");
+        }
+        
     }
 }
 
