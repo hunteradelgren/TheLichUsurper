158d61dc5dcec983d854aa393e90c56c76340871
HunterAdelgren <hunteradelgren@gmail.com>
2021-03-09 14:12:24 -0600

    Spike Trap added

---
+++ TheLichUsurper/Assets/Scripts/Enemy Scripts/SpikeTrap.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    public float damageRate = .5f;
+    public float damage = 1;
+    private float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+                timer = 0;
+                collision.GetComponent<PlayerHealth>().takeDamage(damage);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            timer += Time.deltaTime;
+            if(timer >= damageRate)
+            {
+                timer = 0;
+                collision.GetComponent<PlayerHealth>().takeDamage(damage);
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            timer = 0;
+        }
+    }
+}
